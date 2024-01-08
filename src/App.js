8da49cb40a96b0f4dd1efcb3f7d0cd5009b14d66
seohyon-cho <flanique@naver.com>
2024-01-08 15:04:54 +0900

    리액트 18에서의 automatic batching 개념

--- src/App.js
+++ src/App.js
@@ -13,3 +13,9 @@ function App() {
 	);
 }
 export default App;
+
+/*
+  [ Automatic Batching ]
+  : 여러 개의 state 가 하나의 핸들러 함수 안에서 동시에 변경이 될 때, 그룹으로 묶어서 한 번만 렌더링 처리가 되도록 하는 것. 
+    * ( 리액트 17에서도 Batching 기능이 동작되긴 하나, 만약 Promise를 반환하는 핸들러 함수 안쪽에서 여러 개의 state가 변경이 되면 동작이 되지 않았음. )
+*/
