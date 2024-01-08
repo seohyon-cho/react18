16922599a1b491818145fbd6af0df2432e28350a
seohyon-cho <flanique@naver.com>
2024-01-08 14:47:18 +0900

    Pics, News, Visual 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import News from './components/news/News';
+import Pics from './components/pics/Pics';
+import Visual from './components/visual/Visual';
+import './global.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Visual />
+			<News />
+			<Pics />
+		</div>
+	);
 }
-
 export default App;
---
+++ src/components/news/News.jsx
@@ -0,0 +1,9 @@
+import './News.scss';
+
+export default function News() {
+	return (
+		<section className='News'>
+			<div className='box'></div>
+		</section>
+	);
+}
